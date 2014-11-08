6066d701f9ed03e696b225e6f5a1f58257c2a4ac
TyMaHbl4 <bgulyaev@gmail.com>
2014-11-08 22:36:48 +0200

    All files at once...

---
+++ plot1.R
@@ -0,0 +1,24 @@
+setwd("~/Desktop/Online Coursera/Coursera-Exploratory-Data-Analysis/ExData_Plotting2/")
+unzip("./data/exdata-data-NEI_data.zip", exdir = "./data/")
+# Check if both data exist. If not, load the data.
+if (!"neiData" %in% ls()) {
+    neiData <- readRDS("./data/summarySCC_PM25.rds")
+}
+if (!"sccData" %in% ls()) {
+    sccData <- readRDS("./data/Source_Classification_Code.rds")
+}
+head(neiData)
+head(sccData)
+dim(neiData)
+dim(sccData)
+
+par("mar"=c(5.1, 4.5, 4.1, 2.1))
+png(filename = "./figure/plot1.png", 
+    width = 480, height = 480, 
+    units = "px")
+totalEmissions <- aggregate(neiData$Emissions, list(neiData$year), FUN = "sum")
+# options(scipen=0)
+plot(totalEmissions, type = "l", xlab = "Year", 
+     main = "Total Emissions in the United States from 1999 to 2008", 
+     ylab = expression('Total PM'[2.5]*" Emission"))
+dev.off()
